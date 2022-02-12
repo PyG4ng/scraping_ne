b6eae78d8e1806559f3633d862b95b313c170d44
PyG4ng <91914909+PyG4ng@users.noreply.github.com>
2022-02-12 19:08:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+BASE_URL = "https://habr.com"
+
+HEADERS = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Cookie": "_ga=GA1.2.358192163.1641931160; _ym_uid=1641931161350631546; _ym_d=1641931161; "
+              "__gads=ID=95f6c95f7e5ef2b0:T=1641931160:S=ALNI_MYPkcvJ_2mcxN2HTgAPxtiiyUWGSQ; _ym_isad=1; "
+              "_gid=GA1.2.1947708374.1644601246; hl=ru; fl=ru; visited_articles=651013",
+    "Host": "habr.com",
+    "If-None-Match": 'W/"659-yOLRFWUeHd2QzzAB9Q/OfsTKrgs"',
+    "Referer": "https://habr.com/ru/all/",
+    "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Linux",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 "
+                  "Safari/537.36",
+    "x-app-version": "2.61.0"
+}
---
+++ main.py
@@ -0,0 +1,67 @@
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+from config import BASE_URL, KEYWORDS, HEADERS
+
+
+def scraping_habr(pages):
+    """ Парсит сайт habr.com и выбирает свежие статьи, в которых встречается хотя бы одно из ключевых слов списка
+    KEYWORDS """
+
+    for n in range(1, pages):
+        print(f"Страница {n}")
+        response = requests.get(BASE_URL + f"/ru/all/page{n}/", headers=HEADERS)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        results = soup.find(class_="tm-articles-list").find_all(class_="tm-articles-list__item")
+
+        count = 0
+
+        for el in results:
+
+            date_time = ""
+            if el.find("span", class_="tm-article-snippet__datetime-published"):
+                date_time = el.find("span", class_="tm-article-snippet__datetime-published").text
+
+            link = ""
+            if el.find("a", class_="tm-article-snippet__title-link"):
+                link = "https://habr.com" + el.find("a", class_="tm-article-snippet__title-link").get('href')
+
+            title = ""
+            if el.find("a", class_="tm-article-snippet__title-link"):
+                title = el.find("a", class_="tm-article-snippet__title-link").text
+
+            article_text = ""
+            if el.find(class_="tm-article-body tm-article-snippet__lead"):
+                article_text = el.find(class_="tm-article-body tm-article-snippet__lead").text
+
+            article_text_splited = [el.strip("«()/_-\\*+?!».,") for el in article_text.split()]
+
+            found_word = list(set(article_text_splited) & set(KEYWORDS))
+
+            if found_word:
+                print("\nНайдено ключевое слово: ", "".join(found_word))
+                to_print = f"Заголовок: {title}\nДата: {date_time}\nСсылка: {link}\n"
+                print(to_print)
+                count += 1
+
+        if not count:
+            print("Нет совпадений!\n")
+        time.sleep(1)
+
+
+def main():
+    while True:
+        pages = input("Введите количество страниц которое хотите пролистать (Макс. 50): ")
+        if pages.isdigit() and int(pages) in range(1, 51):
+            pages_to_scrab = int(pages) + 1
+            break
+    print()
+    scraping_habr(pages_to_scrab)
+
+
+if __name__ == "__main__":
+    main()
